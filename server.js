ed26c6149e5d48130a20b59a65b666cb06e74d29
Cnidarian <cmphillips23@gmail.com>
2021-03-25 11:22:47 -0500

    finished parties table

--- server.js
+++ server.js
@@ -17,7 +17,55 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 });
 
-// Get single candidate
+// Get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+    
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get a single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+    
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// Delete parties
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+    
+        res.json({ message: 'successfully deleted', changes: this.changes });
+    });
+});
+
+// Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
                  AS party_name 
@@ -55,6 +103,33 @@ app.get('/api/candidate/:id', (req, res) => {
         });
     });
 }); */
+
+// Update candidates
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    }
+
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+  
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+    
+        res.json({
+            message: 'success',
+            data: req.body,
+            changes: this.changes
+        });
+    });
+});
 
 // Create a candidate
 /* app.post('/api/candidate', ({ body }, res) => {
